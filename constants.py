c6a2042ca7a35240e89e24e9459632a6502fc3d6
aaravmadan <aaravmadan@gmail.com>
2017-04-18 13:22:40 -0700

    Added constants file

---
+++ constants.py
@@ -0,0 +1,13 @@
+NUM_CLASSES = 2
+IMG_SIZE = 227
+FOLD = "0"
+DATA_TYPE = "gender"
+SOURCE_PATH = "../AgeGenderDeepLearning/Folds/train_val_txt_files_per_fold/test_fold_is_"
+LEARNING_RATE=0.0001
+EPOCHS = 2
+BATCH_SIZE = 50
+OPTIMIZER = 'rmsprop'
+FOLDER_NAME = 'TYPE_{}_LR_{}_OPTIMIZER_{}_FOLD_{}'.format(DATA_TYPE, LEARNING_RATE, OPTIMIZER, FOLD)
+LOG_FILE = '{}/status.log'.format(FOLDER_NAME)
+MODEL_FILE = '{}/model.json'.format(FOLDER_NAME)
+WEIGHT_FILE = '{}/weights.h5'.format(FOLDER_NAME)
